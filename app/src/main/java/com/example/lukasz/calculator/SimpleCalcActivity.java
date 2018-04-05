004f84841995feda3cbf68e555fcc7dbafb9a772
Łukasz Okrąglewski <okragly25@gmail.com>
2018-04-05 22:33:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lukasz/calculator/SimpleCalcActivity.java
@@ -0,0 +1,194 @@
+package com.example.lukasz.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SimpleCalcActivity extends AppCompatActivity {
+
+    TextView text;
+    Double memory =0.0;
+    Double operationNumber;
+    boolean isCTapped = false;
+    boolean isInserting = true;
+
+    public enum OperationStatus {
+        OPERATION, CLEAR, EQUALS, ADDITIONING, DIVIDING, SUBSTRUCTING, MULTIPLYING
+    }
+
+    OperationStatus status = OperationStatus.CLEAR;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_calc);
+         text = (TextView)findViewById(R.id.screen);
+    }
+
+    // C/CE
+    public void BackspaceClicked(View view) {
+
+        text.setText("");
+        if (isCTapped)
+        {
+            memory = 0.0;
+            status = OperationStatus.CLEAR;
+            isCTapped = false;
+            return;
+        }
+        isCTapped = true;
+    }
+
+    //SimpleOperations OnClick
+
+    public void Addition(View view)
+    {
+        if(status == OperationStatus.ADDITIONING) return; // causing that spamming with plus don't affect result
+        memory += Double.parseDouble(text.getText().toString());
+        status = OperationStatus.ADDITIONING;
+        isInserting = false;
+    }
+
+    public void Substructing(View view)
+    {
+        if(status == OperationStatus.SUBSTRUCTING) return; // causing that spamming with plus don't affect result
+        memory += Double.parseDouble(text.getText().toString());
+        status = OperationStatus.SUBSTRUCTING;
+        isInserting = false;
+    }
+
+ public void Dividing(View view)
+    {
+        if(status == OperationStatus.DIVIDING) return; // causing that spamming with plus don't affect result
+        memory += Double.parseDouble(text.getText().toString());
+        status = OperationStatus.DIVIDING;
+        isInserting = false;
+    }
+
+ public void Multiplying(View view)
+    {
+        if(status == OperationStatus.MULTIPLYING) return; // causing that spamming with plus don't affect result
+        memory += Double.parseDouble(text.getText().toString());
+        status = OperationStatus.MULTIPLYING;
+        isInserting = false;
+    }
+
+    public void EqualsButton (View view){
+
+        if(isInserting)
+        {
+            operationNumber = Double.parseDouble(text.getText().toString());
+        }
+        isInserting = false;
+        switch (status){
+            case ADDITIONING:  memory += operationNumber; break;
+            case SUBSTRUCTING: memory -= operationNumber; break;
+            case DIVIDING:     memory /= operationNumber; break;
+            case MULTIPLYING:  memory *= operationNumber; break;
+            case CLEAR: memory = operationNumber; break;
+        }
+        text.setText(memory.toString());
+
+
+    }
+
+    // Number buttons onClick methods
+    public void NumberOneClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("1");
+    }
+
+    public void NumberTwoClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("2");
+    }
+
+    public void NumberThreeClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("3");
+    }
+    public void NumberFourClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("4");
+    }
+    public void NumberFiveClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("5");
+    }
+    public void NumberSixClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("6");
+    }
+    public void NumberSevenClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("7");
+    }
+    public void NumberEightClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("8");
+    }
+    public void NumberNineClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("9");
+    }
+    public void NumberZeroClicked(View view)
+    {
+        if(!isInserting)
+        {
+            text.setText("");
+            isInserting = true;
+        }
+        text.append("0");
+    }
+
+
+
+
+}
